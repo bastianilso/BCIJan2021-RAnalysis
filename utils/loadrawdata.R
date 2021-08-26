614e2f7a1c5af07abca9dc51178ae3623e2b09cf
Bastian Ilso <hougaard.junior@gmail.com>
2021-08-26 11:59:11 +0200

    loadrawdata: update to latest version

--- utils/loadrawdata.R
+++ utils/loadrawdata.R
@@ -6,20 +6,23 @@ LoadFromFilePaths <- function(filePathMeta, filePathEvent, filePathSample) {
   dataset_meta <- read.csv(filePathMeta, na.strings="NULL", sep=";")
   dataset_event <- read.csv(filePathEvent, na.strings="NULL", sep=";")
   dataset_sample <- read.csv(filePathSample, na.strings="NULL", sep=";")
-  dataset_meta <- PreprocessMeta(dataset_meta)
+  dataset_meta <- PreprocessMeta(dataset_meta, filePathMeta)
   dataset <- MergeDatasets(dataset_meta, dataset_event, dataset_sample)
   return(dataset)
 }
 
 LoadFromDirectory <- function(dir, event = "Event", sample = "Sample", meta = "Meta", sep=';') {
-  #dir = "HandStrength"
+  #dir = "Kiwi"
   #event = "Game"
   #sample = "Sample"
   #meta = "Meta"
+  #sep = ";"
   #paste("*",event,".csv",sep="")
   #main game event data
+  #exclude <- list.files(path = dir, pattern = "^[^~]")
+  
   df_event <- list.files(recursive=TRUE ,path = dir,
-                      pattern = event,
+                      pattern = paste0(event,"\\.csv$"),
                       full.names = T) %>% 
     tibble(filename = .) %>%   
     mutate(file_contents = map(filename,~ read_delim(file.path(.), delim = sep, na = "NULL", col_types = cols(.default = "c"))))  %>% 
@@ -28,7 +31,7 @@ LoadFromDirectory <- function(dir, event = "Event", sample = "Sample", meta = "M
 
   #sample data
   df_sample <- list.files(recursive=TRUE ,path = dir,
-                            pattern = sample, 
+                            pattern = paste0(sample,"\\.csv$"), 
                             full.names = T) %>% 
     tibble(filename = .) %>%   
     mutate(file_contents = map(filename,~ read_delim(file.path(.), delim = sep, na = "NULL", col_types = cols(.default = "c"))))  %>% 
@@ -37,7 +40,7 @@ LoadFromDirectory <- function(dir, event = "Event", sample = "Sample", meta = "M
   
   #meta data
   df_meta <- list.files(recursive=TRUE ,path = dir,
-                             pattern = meta, 
+                             pattern = paste0(meta,"\\.csv$"), 
                              full.names = T) %>% 
     tibble(filename = .) %>%   
     mutate(file_contents = map(filename,~ read_delim(file.path(.), delim = sep, na = "NULL", col_types = cols(.default = "c"))))  %>% 
@@ -53,11 +56,15 @@ LoadFromDirectory <- function(dir, event = "Event", sample = "Sample", meta = "M
 }
 
 PreprocessMeta <- function(dataset_meta) {
-  print(names(dataset_meta))
+  tryCatch({
   dataset_meta <- dataset_meta %>%
     rename(MetaTimestamp = Timestamp,
            MetaEmail = Email,
            MetaFramecount = Framecount)
+  }, error = function(e) {
+    print(as_tibble(dataset_meta), n = 10)
+    print(e)
+  })
   return(dataset_meta)
 }
 
