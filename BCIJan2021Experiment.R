32159b09dc4a33e857149acacf65686387759034
Bastian Ilso <hougaard.junior@gmail.com>
2021-09-10 13:38:37 +0200

    Visualization: Add Bar Chart, FP asserts etc.

--- BCIJan2021Experiment.R
+++ BCIJan2021Experiment.R
@@ -30,7 +30,7 @@ St <- D %>% ungroup() %>% group_by(GameTitle, Participant,Condition) %>%
             FrustNormalized = unique(FrustNormalized),
             GameOrder = unique(GameOrder),
             gender = unique(Gender),
-            bci_experience = unique(Comment),
+            comment = unique(Comment),
             )
 
 
@@ -59,7 +59,9 @@ St <- St %>% ungroup() %>%
   mutate(rate_trial = (mi_recog_trial)/totalTrials,
          rate_feedback = accInput/ totalTrials,
          session = 1,
-         session = cumsum(session),)
+         session = cumsum(session),
+         bci_experience = ifelse(grepl("BCIExperience",comment), TRUE, FALSE),
+         mi_experience = ifelse(grepl("MIExperience",comment), TRUE, FALSE))
 
 
 
@@ -91,7 +93,7 @@ Sp <- St %>% ungroup() %>% group_by(Participant, GameTitle) %>%
             min_rate = min(rate_trial),
             max_rate = max(rate_trial),
             mean_feedback = mean(rate_feedback),
-            bci_experience = sum(bci_experience == "BCIExperience"),
+            bci_experience = unique(bci_experience),
             time_total = mean(time_total),
             mi_recog_total = sum(mi_recog),
             mi_recog_rest = sum(mi_recog_rest),
@@ -108,9 +110,8 @@ So <- St %>% ungroup() %>% group_by(ConditionOrder) %>%
 #############
 
 Sp_table <- Sp %>% select(bci_experience, mean_rate, min_rate, max_rate, mean_feedback, time_total,
-                          fp_rate_minute) %>%
-                    mutate(bci_experience = ifelse(bci_experience > 0, "Yes", "No"),
-                    bci_experience = ifelse(is.na(bci_experience), "No", bci_experience),
+                          fp_rate_minute, gender, GameTitle) %>%
+                    mutate(bci_experience = ifelse(bci_experience, "Yes", "No"),
                     mean_rate = paste(format(round(mean_rate * 100,0), nsmall = 0),"\\%"),
                     min_rate = paste(format(round(min_rate * 100,0), nsmall = 0),"\\%"),
                     max_rate = paste(format(round(max_rate * 100, 0), nsmall = 0),"\\%"),
@@ -134,6 +135,35 @@ message(paste(Sp_table %>% apply(.,1,paste,collapse=" & "), collapse=" \\\\ "))
   text = paste(text, "\\ ")) %>% V
 
 #############
+# Linear regression models
+#############
+  
+summary(lm(PercNormalized ~ rate_feedback, data = St %>% filter(GameTitle == "HandStrength")))
+summary(lm(PercNormalized ~ rate_trial, data = St %>% filter(GameTitle == "HandStrength")))
+summary(lm(FrustNormalized ~ rate_feedback, data = St))
+summary(lm(FrustNormalized ~ rate_trial, data = St))
+summary(lm(PercNormalized ~ fp_rate_minute, data = St))
+
+
+    
+#############
+# Participants' ratings by Game Title
+#############
+  
+St %>% group_by(GameTitle) %>%
+    summarize(rate_feedback = mean(rate_feedback),
+              rate_trial = mean(rate_trial),
+              PercNormalized = mean(PercNormalized),
+              FrustNormalized = mean(FrustNormalized))
+  
+St %>% group_by(GameOrder, GameTitle) %>%
+  summarize(rate_feedback = mean(rate_feedback),
+            rate_trial = mean(rate_trial),
+            PercNormalized = mean(PercNormalized),
+            FrustNormalized = mean(FrustNormalized)) %>% View()
+
+  
+#############
 # MI recognition by Condition Order
 #############
 
@@ -149,7 +179,39 @@ fig1 <- fig %>%
 orca(fig1, "fig/mi-recognition-by-condition-order.pdf", width=450, height=350)
 
 #############
-# Perceived Control to MI-attempt-based Control (Feedback rate) by Condition
+# Influence of BCI Experience and MI Experience on participant ratings
+#############
+
+St %>% group_by(bci_experience) %>%
+  summarize(rate_trial = mean(rate_trial),
+            rate_feedback = mean(rate_feedback),
+            PercNormalized = mean(PercNormalized),
+            perc_bias = mean(PercNormalized * rate_trial),
+            frust_bias = mean(FrustNormalized * rate_trial),
+            FrustNormalized = mean(FrustNormalized)) %>% View()
+
+St %>% group_by(mi_experience) %>%
+  summarize(rate_trial = mean(rate_trial),
+            rate_feedback = mean(rate_feedback),
+            PercNormalized = mean(PercNormalized),
+            FrustNormalized = mean(FrustNormalized)) %>% View()
+
+
+#############
+# Perceived Control to False Positive Rate
+#############
+
+fig %>%
+  add_trace(data = St, x=~mi_recog, y=~mi_recog_window+mi_recog_rest,
+            type='scatter', mode='markers', color=I('white'), symbol=I('o'),
+            marker=list(size=6, color = 'grey'),
+            hoverinfo='text', text=~paste(Participant, Condition, GameOrder)) %>%
+  layout(showlegend=F, yaxis = list(range=c(-0.05,120), title="Input Window MI", showticklabels=T),
+         xaxis = list(range=c(0,120), title="Rest period MI"))
+
+
+#############
+# Perceived Control to MI rate (ignoring feedback) by Condition
 #############
 FabCurve100A <- St %>% filter(GameTitle %in% c("HandStrength", "Kiwi"),Condition == "100A") %>% select(rate_trial, PercNormalized)
 FabCurve100A.Smooth = supsmu(FabCurve100A$rate_trial, FabCurve100A$PercNormalized, bass=10)
@@ -169,8 +231,6 @@ FabCurve50B <- lm(PercNormalized ~ rate_trial, data = FabCurve50B)
 FabCurve50B <- data.frame(x = (1:100)/100, y = predict(FabCurve50B, data.frame(rate_trial = (1:100)/100)))
 
 
-
-
 fig1 <- fig %>%
   add_trace(name = "100%", data = St %>% filter(GameTitle %in% c("HandStrength", "Kiwi"), Condition == "100A"), x=~rate_trial, y=~jitter(PercNormalized, amount=.02),
             type='scatter',mode='markers', color=I("rgba(0, 0, 0, 0.7)"), symbol=I('circle'), marker=list(size=8)) %>%
@@ -178,7 +238,7 @@ fig1 <- fig %>%
             type='scatter',mode='markers', color=I('black'), symbol=I('o'), marker=list(size=8)) %>%
   add_trace(data=FabCurve100A, x=~x, y=~y, line=list(shape = 'spline'), color=I('black'), type='scatter', mode='lines', showlegend=F) %>%
   add_trace(data=FabCurve100B, x=~x, y=~y, line=list(shape = 'spline'), color=I('grey70'), type='scatter', mode='lines', showlegend=F) %>%
-  layout(yaxis = list(range=c(-0.05,1.1), title="Perceived Control"), xaxis = list(range=c(-0.05,1.1), title="BCI Activation Rate (MI-based)"))
+  layout(yaxis = list(range=c(-0.05,1.1), title="Perceived Control"), xaxis = list(range=c(-0.05,1.1), title="MI Activation"))
 
 fig2 <- fig %>%
   add_trace(name = "50%", data = St %>% filter(GameTitle %in% c("HandStrength", "Kiwi"), Condition == "50A"), x=~rate_trial, y=~jitter(PercNormalized, amount=.02),
@@ -187,13 +247,45 @@ fig2 <- fig %>%
             type='scatter',mode='markers', color=I('black'), symbol=I('o'), marker=list(size=8)) %>%
   add_trace(data=FabCurve50A, x=~x, y=~y, color=I('black'), line=list(shape = 'spline'), type='scatter', mode='line', showlegend=F) %>%
   add_trace(data=FabCurve50B, x=~x, y=~y, color=I('grey70'), line=list(shape = 'spline'), type='scatter', mode='line', showlegend=F) %>%
-  layout(yaxis = list(range=c(-0.05,1.1), title=""), xaxis = list(range=c(-0.05,1.1), title="BCI Activation Rate (MI-based, 50% Limit)"))
+  layout(yaxis = list(range=c(-0.05,1.1), title=""), xaxis = list(range=c(-0.05,1.1), title="MI Activation"))
 
 fig_c <- subplot(fig1, fig2, titleY = TRUE, titleX = TRUE) %>% 
   layout(title = 'Conditions w/ Fab. Input to Control Conditions')
 fig_c
 
-orca(fig_c, "fig/fabricated-input-to-control-trial-activation-smooth.pdf", width=950, height=350)
+orca(fig_c, "fig/fabricated-input-to-control-trial-activation.pdf", width=950, height=350)
+
+
+#############
+# Bar chart depicting ratings
+#############
+
+fig1 <- fig %>%
+  add_trace(data = St %>% filter(GameTitle == "HandStrength") %>% arrange(Participant, ConditionOrder),
+            y=~PercNormalized, type='scatter', mode='markers+lines', color=I('grey80'),
+            marker = list(line = list(width = 0, color = 'rgb(0, 0, 0)'))) %>%
+  add_trace(data = St %>% filter(GameTitle == "HandStrength") %>% arrange(Participant, ConditionOrder),
+            y=~FrustNormalized, type='scatter', mode='markers', color=I('red'),
+            marker = list(line = list(width = 0, color = 'rgb(0, 0, 0)'))) %>%
+  layout(showlegend=F, barmode='overlay', yaxis = list(title="Hand"),
+         xaxis = list(range=c(-1,64), tickmode='array', tickvals=0:63,ticktext=~paste(Participant,Condition), title=""))  
+
+fig2 <- fig %>%
+  add_trace(data = St %>% filter(GameTitle == "Kiwi") %>% arrange(Participant, ConditionOrder),
+            y=~PercNormalized, type='scatter', mode='markers+lines', color=I('grey80'),
+            marker = list(line = list(width = 0, color = 'rgb(0, 0, 0)'))) %>%
+  add_trace(data = St %>% filter(GameTitle == "Kiwi") %>% arrange(Participant, ConditionOrder),
+            y=~FrustNormalized, type='scatter', mode='markers', color=I('red'),
+            marker = list(line = list(width = 0, color = 'rgb(0, 0, 0)'))) %>%
+  layout(showlegend=F, barmode='overlay', yaxis = list(title="Kiwi"),
+         xaxis = list(range=c(-1,64), tickmode='array', tickvals=0:62, ticktext=~paste(Participant,Condition), title=""))  
+
+fig_c <- subplot(fig1, fig2, titleY = TRUE, titleX = TRUE, nrows=2, margin=0.09) %>% 
+  layout(title = 'Participant ratings of Perceived Control (grey) and Frustration (red)')
+fig_c
+
+ orca(fig_c, "fig/participant-rating-analysis-chart.pdf", width=950, height=475)
+
 
 
 #############
@@ -294,171 +386,3 @@ fig_c <- subplot(fig1, fig2, titleY = TRUE, titleX = TRUE) %>%
   layout(title = 'Conditions w/ Fab. Input to Control Conditions')
 
 orca(fig_c, "fig/fabricated-input-to-control.pdf", width=950, height=350)
-
-#############
-# Perceived Control to Level of Control by GameTitle
-#############
-
-#############
-# How much variation was found between peoples control
-#############
-
-
-
-#############
-# Did People improve over time when playing.
-#############
-
-
-
-
-
-
-# # # OLDER CODE # # #
-
-############ Pure Conditions vs Perceived Control ############
-
-qfitPure <- lm(PercNormalized ~ rate, data = St %>% filter(Condition %in% c("50A", "100A")))
-PercRateCurve.Pure <- data.frame(x = (0:100)/100, y = predict(qfitPure, data.frame(rate = (0:100)/100), se.fit=T))
-
-summary(qfitPure)
-
-
-
-fig %>%
-  add_trace(data = trial_bee, x=~x, y=~y, color=I('darkgray'),
-            type="scatter", mode='markers', marker=list(color='rgba(0.3,0.3,0.3,0.5)', line=list(color=I('black'),width=1))) %>%
-  add_trace( data=PercRateCurve.Pure, x=~x, y=~y.fit, type="scatter", mode="lines", color=I('black')) %>%
-  layout(yaxis = list(range=c(0,1), title="Perceived Control"), xaxis=list(range=c(0,1),title="Level of Control"),showlegend=F)
-
-beeswarm::swarmx(x = St$rate, y=St$PercNormalized, xsize=1, ysize=1)
-
-beeswarm::swarmx(x=length(iris$Sepal.Width), y=iris$Sepal.Width)
-
-index <- rep(0, 100)
-str(index)
-values <- rnorm(100)
-str(values)
-plot(index,values)
-
-str(iris$Sepal.Width)
-str(iris$Sepal.Length)
-
-beeswarm::swarmx(length(iris$Sepal.Width), y=iris$Sepal.Width, cex=0.9) %>% View()
-
-beeswarm::beeswarm(x ~ y, data=data.frame(x=iris$Sepal.Width, y=iris$Sepal.Length), cex=0.7,
-                   spacing = 0.1, priority='density')
-
-
-############ Fab+Pure Conditions vs Perceived Control ############
-
-qfitFab <- lm(PercNormalized ~ rate, data = St %>% filter(Condition %in% c("100B", "50B")))
-PercRateCurve.Fab <- data.frame(x = (0:10)/10, y = predict(qfitFab, data.frame(rate = (0:10)/10), se.fit=T))
-
-summary(qfitFab)
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("100B", "50B")), x=~jitter(rate_feedback, amount=.03), y=~jitter(PercNormalized, amount=.03),
-            type="scatter", mode='markers', color=I('orange')) %>%
-  add_trace( data=PercRateCurve.Fab, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  add_trace( data=PercRateCurve.Pure, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  add_trace(data = St %>% filter(Condition %in% c("100A", "50A")), x=~jitter(rate_feedback, amount=.03), y=~jitter(PercNormalized, amount=.03),
-            type="scatter", mode='markers', color=I('SeaGreen')) %>%
-  layout(yaxis = list(range=c(0,1), title="Perceived Control"), xaxis=list(range=c(0,1),title="Rate"),showlegend=F)
-
-############ Pure Conditions vs Frustration ############
-
-qfrustPure <- lm(FrustNormalized ~ rate, data = St %>% filter(Condition %in% c("100A", "50A")))
-FrustRateCurve.Pure <- data.frame(x = (0:100)/100, y = predict(qfrustPure, data.frame(rate = (0:100)/100), se.fit=T))
-
-summary(qfrustPure)
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("100A", "50A")), x=~jitter(rate_feedback, amount=.03), y=~jitter(FrustNormalized, amount=.03), color=I('darkgray'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  add_trace( data=FrustRateCurve.Pure, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  layout(yaxis = list(range=c(0,1), title="Frustration"), xaxis=list(range=c(0,1),title="Rate"),showlegend=F)
-
-############ Pure+Fab Conditions vs Frustration ############
-
-qfrustFab <- lm(FrustNormalized ~ rate, data = St %>% filter(Condition %in% c("100B", "50B")))
-FrustRateCurve.Fab <- data.frame(x = (0:10)/10, y = predict(qfrustFab, data.frame(rate = (0:10)/10), se.fit=T))
-
-summary(qfrustFab)
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("50B")), x=~jitter(rate, amount=.03), y=~jitter(FrustNormalized, amount=.03),
-            type="scatter", mode='markers', color=I('orange')) %>%
-  add_trace( data=FrustRateCurve.Fab, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  add_trace( data=FrustRateCurve.Pure, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  add_trace(data = St %>% filter(Condition %in% c("50A")), x=~jitter(rate, amount=.03), y=~jitter(FrustNormalized, amount=.03),
-            type="scatter", mode='markers', color=I('SeaGreen')) %>%
-  layout(yaxis = list(range=c(0,1), title="Frustration"), xaxis=list(range=c(0,1),title="Rate"),showlegend=F)
-
-############ Perceived Control vs Frustration ############
-
-qfrustPerc <- lm(FrustNormalized ~ rate, data = St)
-FrustPercCurve <- data.frame(x = (0:10)/10, y = predict(qfrustPerc, data.frame(rate = (0:10)/10), se.fit=T))
-summary(qfrustPerc)
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("50B","100B")), x=~jitter(PercNormalized, amount=.02), y=~jitter(FrustNormalized, amount=.02), color=I('orange'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  add_trace(data = St %>% filter(Condition %in% c("50A","100A")), x=~jitter(PercNormalized, amount=.02), y=~jitter(FrustNormalized, amount=.02), color=I('seagreen'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  add_trace( data=FrustPercCurve, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  layout(yaxis = list(range=c(0,1), title="Frustration"), xaxis=list(range=c(0,1),title="Perceived Control"),showlegend=F)
-
-
-# We need to do an aggregate frustration thing as a follow-up to do, based on the order people rated. to improve
-# the rating between frustration and the fabricated.
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("50B")), x=~jitter(rate, amount=.02), y=~jitter(PercNormalized, amount=.02), color=I('skyblue'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  add_trace(data = St %>% filter(Condition %in% c("100A")), x=~jitter(rate, amount=.02), y=~jitter(PercNormalized, amount=.02), color=I('seagreen'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  layout(yaxis = list(range=c(0,1), title="Frustration"), xaxis=list(range=c(0,1),title="Perceived Control"),showlegend=F)
-
-
-qfitAll <- lm(rate ~ PercNormalized, data = St)
-PercRateCurve.All <- data.frame(x = (0:10)/10, y = predict(qfitAll, data.frame(PercNormalized = (0:10)/10), se.fit=T))
-
-fig %>%
-  add_trace(data = St, x=~jitter(rate, amount=.03), y=~jitter(PercNormalized, amount=.03), color=I('darkgray'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  #add_trace( data=PercRateCurve.All, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  layout(yaxis = list(range=c(0,1), title="Perceived Control"), xaxis=list(range=c(0,1),title="Rate"),showlegend=F)
-
-FfitAll <- lm(rate ~ FrustNormalized, data = St)
-FrustRateCurve.All <- data.frame(x = (0:10)/10, y = predict(FfitAll, data.frame(PercNormalized = (0:10)/10), se.fit=T))
-
-fig %>%
-  add_trace(data = St, x=~jitter(PercNormalized, amount=.03), y=~jitter(FrustNormalized, amount=.03), color=I('darkgray'),
-            type="scatter", mode='markers', color=~Participant) %>%
-  #add_trace( data=PercRateCurve.All, x=~x, y=~y.fit, type="scatter", mode="lines") %>%
-  layout(yaxis = list(range=c(0,1), title="Frustration"), xaxis=list(range=c(0,1),title="Perceived Control"),showlegend=F)
-
-timetemplate %>%
-  add_trace(data = df_kiwi %>% filter(Participant == "1") %>% arrange(Framecount), x =~Framecount, y=~BCIConfidence, type="scattergl", mode="markers+lines")
-
-
-
-
-
-#################################################
-# Influence of 50A on people's level of control #
-#################################################
-
-fig %>%
-  add_trace(data = St, x=~rate, y=~jitter(PercNormalized, amount=.03),
-            type='scattergl',mode='markers', color=~Condition, size=2)
-
-
-############ Horizontal Box Plot ############
-
-fig %>%
-  add_trace(data = St %>% filter(Condition %in% c("100A")), x=~rate, y=~as.factor(PerceivedControlEpisode), color=I('black'),
-            type="box", boxpoints="all", jitter=.02, marker=list(color='rgba(0.3,0.3,0.3,0.5)', line=list(color=I('black'),width=1)))
-  #add_trace( data=PercRateCurve.Pure, x=~x, y=~y.fit, type="scatter", mode="lines", color=I('black')) %>%
-  #layout(yaxis = list(range=c(0,1), title="Perceived Control"), xaxis=list(range=c(0,1),title="Level of Control"),showlegend=F)
