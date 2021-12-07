f97facd4e442fe76436c659bfc195836264b70b0
Bastian Ilso <hougaard.junior@gmail.com>
2021-12-07 15:59:01 +0100

    visutils: dont create line larger than min/max
    Even if we plot from 0 to 1, our regression Lines
    would be skewed if we allowed them to go from 0-1.
    So the lines should always have no more length than
    the data provided.

--- utils/visutils.R
+++ utils/visutils.R
@@ -1,10 +1,12 @@
 # p_lin: Predict Linear
 # Returns a 100 row (x,y) data frame, which describe a line.
 p_lin <- function(df, response, term) {
+  min = min(df[[term]]) * 100
+  max = max(df[[term]]) * 100
   fm <- lm(as.formula(paste(response, "~", term)), data = df)
-  df_p <- data.frame(x = (1:100)/100)
-  df_p[[term]] <- (1:100)/100
-  curve <- data.frame(x = (1:100)/100, y = predict(fm, df_p))
+  df_p <- data.frame(x = (min:max)/100)
+  df_p[[term]] <- (min:max)/100
+  curve <- data.frame(x = (min:max)/100, y = predict(fm, df_p))
   return(curve)
 }
 
